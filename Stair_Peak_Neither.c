1c3c48da76bc96f00c117bed6330f600f36c852d
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-03-30 01:51:48 +0600

    Codeforces

---
+++ Stair_Peak_Neither.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+
+        if (a < b && b < c)
+            printf("STAIR\n");
+        else if (a < b && b > c)
+            printf("PEAK\n");
+        else
+            printf("NONE\n");
+    }
+
+    return 0;
+}
