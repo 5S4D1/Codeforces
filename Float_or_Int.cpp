cc95c3187ef6b7a9bee4ebef9fe72e3eae472368
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-04-28 17:01:50 +0600

    Float or int

---
+++ Float_or_Int.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float n;
+    cin >> n;
+    int a = (int)n;
+
+    if (n == a)
+    {
+        cout << "int " << a << endl;
+    }
+    else
+    {
+        float b = n - a;
+        cout << "float " << a << " ";
+        printf("%.3f\n",b);
+    }
+    return 0;
+}
