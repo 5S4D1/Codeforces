4065aa4d2243fa53d2e2dc503d1f80880d58b1b7
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-04-29 21:28:13 +0600

    Sort Number

---
+++ Sort_Number.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    if (a <= b && a < c)
+    {
+        cout << a << endl;
+        if (b < c)
+        {
+            cout << b << endl;
+            cout << c << endl;
+        }
+        else
+        {
+            cout << c << endl;
+            cout << b << endl;
+        }
+    }
+    else if (b < a && b <= c)
+    {
+        cout << b << endl;
+        if (a < c)
+        {
+            cout << a << endl;
+            cout << c << endl;
+        }
+        else
+        {
+            cout << c << endl;
+            cout << a << endl;
+        }
+    }
+    else if (c <= a && c < b)
+    {
+        cout << c << endl;
+        if (a < b)
+        {
+            cout << a << endl;
+            cout << b << endl;
+        }
+        else
+        {
+            cout << b << endl;
+            cout << a << endl;
+        }
+    }
+    else
+    {
+        if (a == b && b == c)
+        {
+            cout << a << endl;
+            cout << b << endl;
+            cout << c << endl;
+        }
+    }
+    cout << endl;
+    cout << a << "\n"
+         << b << "\n"
+         << c << endl;
+    return 0;
+}
