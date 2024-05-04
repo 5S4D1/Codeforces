e88dfc269b04b88d41c2a9f98ed9e61d8145d69f
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-05-04 14:57:09 +0600

    Codeforces

---
+++ Maximize.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        // If x is prime, then y = 1 will maximize gcd(x, y) + y
+        // Otherwise, y = x - 1 will maximize gcd(x, y) + y
+        int y = (x == 2) ? 1 : x - 1;
+
+        cout << y << endl;
+    }
+
+    return 0;
+}
