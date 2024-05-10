57b2d00556731115ddc2db9114c2fcf68b5d953b
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-05-10 23:33:27 +0600

    Codeforces

---
+++ MyFirstSortingProblem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    int i = 0;
+    while (i < t)
+    {
+        int x, y;
+        cin >> x >> y;
+        if (x < y)
+        {
+            cout << x << " " << y << endl;
+        }
+        else
+            cout << y << " " << x << endl;
+
+        i++;
+    }
+
+    return 0;
+}
