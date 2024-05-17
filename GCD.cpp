c94e135f8a27f902906f5e03f39cc1c1946576c8
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-05-17 23:56:33 +0600

    Codeforces

---
+++ GCD.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x , y, i;
+    cin >> x >> y;
+    if(x>y)
+        i = y;
+    else
+        i = x;
+    while (i>0)
+    {
+        if(x%i == 0 && y%i == 0)
+        {
+            cout << i;
+            break;
+        }
+        i--;
+    }
+    
+
+    return 0; 
+}
