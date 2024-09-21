9acbfc3272e695a5e75ce959336ac5136e6a6c03
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-09-22 00:33:27 +0600

    codeforces

---
+++ Robin_Helps.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int robin = 0;
+        int help = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int temp;
+            cin >> temp;
+            if (temp == 0 && robin > 0)
+            {
+                robin--;
+                help++;
+            }
+            if (temp >= k)
+                robin += temp;
+        }
+
+        cout << help << endl;
+    }
+
+    return 0;
+}
