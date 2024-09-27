d5998edbb4780989189674b37692cb5994e3ef86
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-09-27 23:33:54 +0600

    codeforces

---
+++ Chat_Room.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string text, s = "hello";
+    int comp = 0;
+    cin >> text;
+    int len = text.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (text[i] == s[comp])
+            comp++;
+    }
+    if (comp == 5)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
