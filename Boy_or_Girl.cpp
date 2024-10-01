c95c4a2b5b8a9425369c7404c8f2e12772bef9ca
Sadi <123616359+Sadi86@users.noreply.github.com>
2024-10-02 00:26:55 +0600

    codeforces

---
+++ Boy_or_Girl.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    int indistinct = 0;
+    int len = name.length();
+
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (name[i] == name[j]){
+                indistinct++;
+                break;
+            }
+        }
+    }
+    int new_len = len - indistinct;
+
+    if (new_len % 2 != 0)
+        cout << "IGNORE HIM!" << endl;
+    else
+        cout << "CHAT WITH HER!" << endl;
+
+    return 0;
+}
